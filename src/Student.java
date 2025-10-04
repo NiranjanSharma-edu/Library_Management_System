c1e2f33597403a24e66552163372d48da235cc11
Niranjan <niranjansharma27nj@gmail.com>
2025-10-04 15:41:14 +0530

    Added Student model class

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Student {
+    String name;
+    int id_no;
+    String stream;
+    String book1 = "";
+    String book2 = "";
+    int book_no = 0;
+
+    public Student(String name, int id_no, String stream) {
+        this.name = name;
+        this.id_no = id_no;
+        this.stream = stream;
+    }
+}
